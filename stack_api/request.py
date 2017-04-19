8d227d6d06642cde76031f6d2f8da07f78f0e86e
ei08047 <ei08047@fe.up.pt>
2017-04-19 17:10:38 +0100

    example filter and request sugestions

--- stack_api/request.py
+++ stack_api/request.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+#questions/unanswered
+#questions/unanswered/my-tags
+#/api/questions/featured
+#/api/users/{ids}/posts  Get all posts (questions and answers) owned by a set of users.
+
+#/api/questions/{ids}/linked  Get the questions that link to the questions identified by a set of ids.
+#/api/questions/{ids}/related  Get the questions that are related to the questions identified by a set of ids.
+
+#/api/users/{id}/top-answer-tags  Get the top tags (by score) a single user has posted answers in.
+/api/users/{id}/tags/{tags}/top-answers  Get the top answers a user has posted on questions with a set of tags.
+
+
+
 import requests
 import json
 import sys
